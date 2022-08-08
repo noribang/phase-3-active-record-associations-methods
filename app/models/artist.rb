86d6e1d37b98b76b9f2b2ad0108a50c97934e9aa
hoonkim <hoonk9387@gmail.com>
2022-08-07 19:56:08 -0500

    Completed assignment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,26 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    # return artist array of all songs
+    # self.songs
+
+    # return artist first song
+    self.songs.first
+  end
+
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.genres.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
   end
 end
